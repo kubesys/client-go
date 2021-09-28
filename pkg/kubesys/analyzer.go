353c7d19510c397ddcc23267613953986cd61d7b
oneflyingfish <a_flying_fish@outlook.com>
2021-09-27 21:56:05 -0500

    fix read-group bug

--- pkg/kubesys/analyzer.go
+++ pkg/kubesys/analyzer.go
@@ -12,48 +12,49 @@ import (
  *      date  : 2021/4/8
  */
 type KubernetesAnalyzer struct {
-	KindToFullKindMapper              map[string][]string
-	FullKindToApiPrefixMapper         map[string]string
+	KindToFullKindMapper      map[string][]string
+	FullKindToApiPrefixMapper map[string]string
 
-	FullKindToNameMapper              map[string]string
-	FullKindToNamespaceMapper         map[string]bool
+	FullKindToNameMapper      map[string]string
+	FullKindToNamespaceMapper map[string]bool
 
-	FullKindToVersionMapper           map[string]string
-	FullKindToGroupMapper             map[string]string
-	FullKindToVerbsMapper             map[string]interface{}
+	FullKindToVersionMapper map[string]string
+	FullKindToGroupMapper   map[string]string
+	FullKindToVerbsMapper   map[string]interface{}
 }
 
 func NewKubernetesAnalyzer() *KubernetesAnalyzer {
 	analyzer := new(KubernetesAnalyzer)
 
-	analyzer.KindToFullKindMapper      = make(map[string][]string)
+	analyzer.KindToFullKindMapper = make(map[string][]string)
 	analyzer.FullKindToApiPrefixMapper = make(map[string]string)
 
-	analyzer.FullKindToNameMapper      = make(map[string]string)
+	analyzer.FullKindToNameMapper = make(map[string]string)
 	analyzer.FullKindToNamespaceMapper = make(map[string]bool)
 
-	analyzer.FullKindToVersionMapper   = make(map[string]string)
-	analyzer.FullKindToGroupMapper     = make(map[string]string)
-	analyzer.FullKindToVerbsMapper     = make(map[string]interface{})
+	analyzer.FullKindToVersionMapper = make(map[string]string)
+	analyzer.FullKindToGroupMapper = make(map[string]string)
+	analyzer.FullKindToVerbsMapper = make(map[string]interface{})
 
 	return analyzer
 }
 
-func (analyzer *KubernetesAnalyzer) Learning (client KubernetesClient) {
-	registryRequest,_ := client.CreateRequest("GET", client.Url, nil)
+func (analyzer *KubernetesAnalyzer) Learning(client KubernetesClient) {
+	registryRequest, _ := client.CreateRequest("GET", client.Url, nil)
 	registryValues, _ := client.RequestResource(registryRequest)
+
+	// fmt.Println(registryRequest)
+
 	for _, v := range registryValues["paths"].([]interface{}) {
 		path := v.(string)
-		if strings.HasPrefix(path, "/api") &&
-			(len(strings.Split(path, "/")) == 4 ||
-				strings.EqualFold(path, "/api/v1")) {
-			resourceRequest,_ := client.CreateRequest("GET", client.Url + path, nil)
+		if strings.HasPrefix(path, "/api") && (len(strings.Split(path, "/")) == 4 || strings.EqualFold(path, "/api/v1")) {
+			resourceRequest, _ := client.CreateRequest("GET", client.Url+path, nil)
 			resourceValues, _ := client.RequestResource(resourceRequest)
 			apiVersion := resourceValues["groupVersion"].(string)
 			for _, w := range resourceValues["resources"].([]interface{}) {
 				resourceValue := w.(map[string]interface{})
 
-				shortKind  := resourceValue["kind"].(string)
+				shortKind := resourceValue["kind"].(string)
 				fullKind := getFullKind(resourceValue, shortKind, apiVersion)
 
 				if _, ok := analyzer.FullKindToApiPrefixMapper[fullKind]; !ok {
@@ -64,7 +65,7 @@ func (analyzer *KubernetesAnalyzer) Learning (client KubernetesClient) {
 					analyzer.FullKindToNamespaceMapper[fullKind] = resourceValue["namespaced"].(bool)
 
 					analyzer.FullKindToVersionMapper[fullKind] = apiVersion
-					analyzer.FullKindToGroupMapper[fullKind] = getGroup(client.Url + path)
+					analyzer.FullKindToGroupMapper[fullKind] = getGroup(apiVersion)
 					analyzer.FullKindToVerbsMapper[fullKind] = resourceValue["verbs"]
 				}
 			}
@@ -72,14 +73,10 @@ func (analyzer *KubernetesAnalyzer) Learning (client KubernetesClient) {
 	}
 }
 
-func getGroup(url string) string {
-	if strings.HasSuffix(url, "/api/v1") {
-		return ""
-	}
-	stx := strings.LastIndex(url, "/")
-	etx := strings.LastIndex(url[0:stx], "/")
-	if etx > stx {
-		return url[stx+1: etx]
+func getGroup(apiVersion string) string {
+	index := strings.LastIndex(apiVersion, "/")
+	if index > 0 {
+		return apiVersion[0:index]
 	}
 	return ""
 }
